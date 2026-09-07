7e68f60bbd752f90cd5f1bf194f9b25047fceec1
Siddharth verma <siddharth26338@gmail.com>
2026-09-07 11:52:18 +0530

    Time: 8 ms (54.23%), Space: 22.9 MB (58.69%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
 
-    bool caneatall(vector<int>& piles, int mid, int h){
-        double actualhr =0;
-       for( int x: piles){
-        actualhr += x/mid; 
-
-        if(x%mid != 0){
+bool caneatall(vector<int>& piles,int mid, int h){
+    double actualhr =0;
+    for(int i : piles){
+        actualhr += i/mid;
+        if(i%mid != 0){
             actualhr++;
         }
 
-       }
-       return actualhr <= h;
     }
+        return actualhr<=h;
+
+}
 
     int minEatingSpeed(vector<int>& piles, int h) {
-        int n = piles.size();
-        double mid = 0;
+        int n =piles.size();
+        int l = 1;
 
-        int l= 1;
         int r = *max_element(piles.begin(),piles.end());
 
+    int mid =0;
         while(l<=r){
             mid = l + (r-l)/2;
 
             if(caneatall(piles,mid,h)){
-                r=mid-1;
+                r = mid-1;
             }
-            else l = mid +1;
+            else l= mid +1;
+
         }
-return l;
+        return l;
     }
 };
