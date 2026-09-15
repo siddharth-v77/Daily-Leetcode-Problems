24c11b9961dfdf2a82be66d788d8bbc3d5a43757
Siddharth verma <siddharth26338@gmail.com>
2026-09-15 11:45:56 +0530

    Time: 0 ms (100%), Space: 19.4 MB (88.05%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -13,17 +13,17 @@ public:
     ListNode* mergeTwoLists(ListNode* head1, ListNode* head2) {
         if(head1 == NULL || head2 == NULL){
             return head1 == NULL ? head2 : head1 ;
-        }
+        } 
 
         if(head1->val <= head2->val){
-            head1->next = mergeTwoLists(head1->next,head2);
+            head1->next = mergeTwoLists(head1->next , head2);
             return head1;
         }
-
-        else{
+        else {
             head2->next = mergeTwoLists(head1, head2->next);
             return head2;
         }
+        return NULL;
+        
     }
-    
 };
